065d3aaf3b87e0d85a9ec8ac5b3e9e20944ebb5c
Al Fikri Kholil Misbah <44604935+Al-User12@users.noreply.github.com>
2023-06-07 23:46:16 +0700

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -79,74 +79,12 @@ export default function Dashboard({ constructor }) {
         constructor()
     }, [])
 
-    useEffect(() => {
-        async function getStar() {
-            let star = await fetch("https://api.github.com/repos/madvier83/next-kasir")
-            star = await star.json()
-            setStargazer(star.stargazers_count)
-        }
-        getStar()
-    }, [])
-
     return (
         <>
             <Head>
                 <title>CookiePOS | Dashboard</title>
             </Head>
             <GlobalDataProvider>
-                <div className="flex flex-col">
-                    
-                    <div className={`flex ${setup.history?"flex-col-reverse":"flex-col"}`}>
-
-                        <div className="m-4 mb-8 flex flex-col md:flex-row max-w-4xl select-none">
-
-                            <div className="stat">
-                                <div className="stat-figure text-primary">
-
-                                    <a href="https://github.com/Al-User12" target="_blank" rel='noreferrer'>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            className="inline-block w-8 h-8 stroke-current"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                            ></path>
-                                        </svg>
-                                    </a>
-                                </div>
-                                <div className="stat-title">GitHub Stars</div>
-                                    <div className="stat-value text-primary">
-                                        {stargazer}
-                                    </div>
-                                <div className="stat-desc">0% more than last month</div>
-                            </div>
-
-                            <div className="stat">
-                                <div className="stat-figure text-secondary">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        className="inline-block w-8 h-8 stroke-current"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M13 10V3L4 14h7v7l9-11h-7z"
-                                        ></path>
-                                    </svg>
-                                </div>
-                                <div className="stat-title">Page Views</div>
-                                <div className="stat-value text-secondary">0.1K</div>
-                                <div className="stat-desc">0% more than last month</div>
-                            </div>
-
                             <div className="stat">
                                 <div className="stat-figure text-secondary">
                                     <div className="avatar online">
@@ -157,8 +95,8 @@ export default function Dashboard({ constructor }) {
                                         </a>
                                     </div>
                                 </div>
-                                <div className="stat-value">100%</div>
-                                <div className="stat-title"> Open Source</div>
+                                <div className="stat-value">Enjoy this app</div>
+                                <div className="stat-title"> POS Application with new stack </div>
                                 <a
                                     target="_blank"
                                     href="https://github.com/Al-User12"
@@ -177,8 +115,8 @@ export default function Dashboard({ constructor }) {
                                         <div className="card-body">
                                             <h2 className="card-title">Welcome!</h2>
                                             <p>
-                                                Welcome to CookiePOS, open
-                                                source point of sales app!
+                                                Welcome to CookiePOS, Production of
+                                                point of sales app!
                                             </p>
                                             <div className="card-actions justify-end">
                                                 <button
